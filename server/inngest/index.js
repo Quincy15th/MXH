9bbd564386a0f4e24036e3f414d7e09bbcc004ea
Quincy15th <anh751715@gmail.com>
2026-03-11 14:18:48 +0700

    fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -9,8 +9,9 @@ const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_address, image_url } = event.data;
-    let username = email_address[0].email_address.split("@")[0];
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    let username = email_addresses[0].email_address.split("@")[0];
     const user = await User.findOne({ username });
     if (user) {
       username = username + Math.floor(Math.random() * 10000);
